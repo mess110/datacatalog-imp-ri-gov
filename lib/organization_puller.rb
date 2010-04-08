f955ebe383a1748f6ac712cd51c1d78ba6af17f0
Cristian Mircea Messel <mess110@gmail.com>
2010-04-08 15:45:32 +0200

    updated to use the importer

---
+++ lib/organization_puller.rb
@@ -0,0 +1,49 @@
+require File.dirname(__FILE__) + '/puller'
+require File.dirname(__FILE__) + '/output'
+
+class OrganizationPuller < Puller
+
+	def initialize
+		@parse_file	= Output.dir "/../cache/parsed/organization"
+		super
+	end
+
+	protected
+
+	#[organization.size,s site_category, sos_entity_id, org_title, title_abbrev, title_alpha, hostname]
+	def parse_metadata
+		#now we need to parse each individual peace of data
+		#ok now think. I don't have any direct xml link so I am actually gathering links that point to a website that points to usefull data
+		@organization.each do |data|
+			source = 
+			{
+				:catalog_url => "http://ri.gov/data",
+				:description => "Plans, develops, and implements the use of new technology for District agencies to improve delivery of services to residents, businesses and visitors.",
+				:home_url => data[6],
+				:name => data[3],
+				:org_type => "governmental",
+				:organization => {
+					:name => "District of Columbia,",
+				},
+				:url => "http://ri.gov/data",
+				#:active_organizations
+				#:jurisditcions
+			}
+#--- 
+#:catalog_url: http://dc.gov/agencies
+#:description: Offers comprehensive, integrated healthcare to medically underserved residents in Washington, DC.
+#:home_url: http://www.greatersoutheast.org
+#:name: Greater Southeast Community Hospital
+#:org_type: governmental
+#:organization: 
+#  :name: District of Columbia
+#:url: http://dc.gov/agencies/detail.asp?id=1030
+
+			yml_file = @parse_file + ("/%08i.yml" % data[0])
+			#no timestamps here so caching is a little more primitive
+			#write source if it is different from cached version
+			U.write_yaml(yml_file, source) if U.fetch_yaml(yml_file) != source rescue U.write_yaml(yml_file, source)
+		end
+	end
+
+end
---
+++ lib/source_puller.rb
@@ -0,0 +1,53 @@
+require File.dirname(__FILE__) + '/puller'
+require File.dirname(__FILE__) + '/output'
+
+class SourcePuller < Puller
+
+	def initialize
+		@parse_file	= Output.dir "/../cache/parsed/source"
+		super
+	end
+
+	protected
+
+	#[source.size, date_inserted, description, hostname, linkk, title, title_iwantto, updatedat, tags, org_title, site_category]
+	def parse_metadata
+		#now we need to parse each individual peace of data
+		#ok now think. I don't have any direct xml link so I am actually gathering links that point to a website that points to usefull data
+		@source.each do |data|
+			source = 
+			{
+				:catalog_name	=>	@catalog_name,
+				:catalog_url	=>	@catalog_url, 
+				:description	=>	data[2],
+				#:downloads		=> "none",
+				:frequency		=>	"unknown",
+				:organization	=>	{
+					:home_url	=>	data[3],
+					:name		=>	data[9],
+				},
+				#:site_category =>	data[10], 	#optional
+				:released		=>	data[1],
+				#:updated		=>	data[7],	#optional
+				:source_type	=>	"dataset",
+				:title			=>	data[5],
+				:url			=>	data[4],
+				#:tags			=>	data[8],
+			}
+
+			yml_file = @parse_file + ("/%08i.yml" % data[0])
+			#if the file was not saved before save it
+			#if it was saved before and the saved timestamp is older than the new one, change it
+			#caching is handled by last update :)
+			begin
+				backthen = Time.parse(YAML::load(File.open(yml_file))[:updated])
+				rightnow = Time.parse(data[7])
+				if (backthen < rightnow)
+					U.write_yaml(yml_file, source)
+				end
+			end rescue U.write_yaml(yml_file, source)
+			U.write_yaml(yml_file, source) #erase this after
+		end
+	end
+
+end

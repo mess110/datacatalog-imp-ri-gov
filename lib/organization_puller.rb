5046079c43e0848dc0a7660e5fd446b70390aa29
Cristian Mircea Messel <mess110@gmail.com>
2010-04-12 16:05:09 +0200

    still roll back. should check how to roll back more than once Revert "Revert "updated whitespace and string interpolation""
    This reverts commit 095ad25cc03b2d2b7250712ec938360b6cca71d2.

--- lib/organization_puller.rb
+++ lib/organization_puller.rb
@@ -3,41 +3,38 @@ require File.dirname(__FILE__) + '/output'
 
 class OrganizationPuller < Puller
 
-	def initialize
-		@parse_file	= Output.dir "/../cache/parsed/organization"
-		super
-	end
+  def initialize
+    @parse_file  = Output.dir "/../cache/parsed/organization"
+    super
+  end
 
-	protected
+  protected
 
-	def parse_metadata
-		#now we need to parse each individual peace of data
-		#ok now think. I don't have any direct xml link so I am actually gathering links that point to a website that points to usefull data
-		@organization.each do |data|
-			source = 
-			{
-				:organization => {
-					:name => "District of Rhode Island"
-				},
-				:catalog_url => "http://ri.gov/data/agencies",
-				:description => "",
-				:home_url => "http://" + data[:hostname],
-				:name => data[:org_title],
-				:org_type => "governmental",
-				:url => "http://" + data[:hostname],
-				#:active_organizations
-				#:jurisditcions
-			}
+  def parse_metadata
+    #now we need to parse each individual peace of data
+    #ok now think. I don't have any direct xml link so I am actually gathering links that point to a website that points to usefull data
+    @organization.each do |data|
+      source = 
+      {
+        :organization  => { :name => "District of Rhode Island" },
+        :catalog_url   => "http://ri.gov/data/agencies",
+        :description   => "",
+        :home_url      => "http://" + data[:hostname],
+        :name          => data[:org_title],
+        :org_type      => "governmental",
+        :url           => "http://" + data[:hostname],
+      }
 
-			#don't upload organizations that don't have a name
-			if source[:name] != ""
-				#the file path which we are working on
-				yml_file = @parse_file + ("/%08i.yml" % data[:id].to_s)
-				#no timestamps here so caching is a little more primitive
-				#write source if it is different from cached version
-				U.write_yaml(yml_file, source) if U.fetch_yaml(yml_file) != source rescue U.write_yaml(yml_file, source)
-			end
-		end
-	end
+      #don't upload organizations that don't have a name
+      if source[:name] != ""
+        #the file path which we are working on
+        yml_file = "%s/%08i.yml" % [@parse_file, data[:id]]
+        #no timestamps here so caching is a little more primitive
+        #write source if it is different from cached version
+
+        U.write_yaml(yml_file, source) if U.fetch_yaml(yml_file) != source rescue U.write_yaml(yml_file, source)
+      end
+    end
+  end
 
 end
--- lib/source_puller.rb
+++ lib/source_puller.rb
@@ -3,63 +3,63 @@ require File.dirname(__FILE__) + '/output'
 
 class SourcePuller < Puller
 
-	def initialize
-		@parse_file	= Output.dir "/../cache/parsed/source"
-		super
-	end
+  def initialize
+    @parse_file  = Output.dir "/../cache/parsed/source"
+    super
+  end
 
-	protected
+  protected
 
-	def parse_metadata
-		#now we need to parse each individual peace of data
-		#ok now think. I don't have any direct xml link so I am actually gathering links that point to a website that points to usefull data
-		@source.each do |data|
-			#
-			release_time = Time.parse(data[:date_inserted])
+  def parse_metadata
+    #now we need to parse each individual peace of data
+    #ok now think. I don't have any direct xml link so I am actually gathering links that point to a website that points to usefull data
+    @source.each do |data|
+      #
+      release_time = Time.parse(data[:date_inserted])
 
-			source = 
-			{
-				:catalog_name	=>	@catalog_name,
-				:catalog_url	=>	@catalog_url, 
-				:description	=>	data[:description],
-				:frequency		=>	"unknown",
-				:organization	=>	{
-					:home_url	=>	data[:hostname],
-					:name		=>	data[:org_title],	#organization title
-				},
-				:released		=>	{
-					:day		=> release_time.strftime("%d").to_i,
-					:month		=> release_time.strftime("%m").to_i,
-					:year		=> release_time.strftime("%Y").to_i,
-				},
-				:source_type	=>	"dataset",
-				:title			=>	data[:title],
-				:url			=>	data[:url],
-			}
+      source = 
+      {
+        :catalog_name    =>  @catalog_name,
+        :catalog_url     =>  @catalog_url, 
+        :description     =>  data[:description],
+        :frequency       =>  "unknown",
+        :organization    =>  {
+          :home_url        =>  data[:hostname],
+          :name            =>  data[:org_title],  #organization title
+        },
+        :released        => {
+          :day             =>   release_time.strftime("%d").to_i,
+          :month           =>   release_time.strftime("%m").to_i,
+          :year            =>   release_time.strftime("%Y").to_i,
+        },
+        :source_type     =>  "dataset",
+        :title           =>  data[:title],
+        :url             =>  data[:url],
+      }
 
-			if source[:organization][:name] == ""
-				source[:organization][:name] = "District of Rhode Island"
-			end
+      if source[:organization][:name] == ""
+        source[:organization][:name] = "District of Rhode Island"
+      end
 
-			if source[:title] == ""
-				source[:title] = "Rhode Island Service"
-			end
+      if source[:title] == ""
+        source[:title] = "Rhode Island Service"
+      end
 
-			#the file path which we are working on
-			yml_file = @parse_file + ("/%08i.yml" % data[:id].to_s)
-			#if the file was not saved before save it
-			#if it was saved before and the saved timestamp is older than the new one, change it
-			#caching is handled by last update :)
-			begin
-				back_then = Time.parse(YAML::load(File.open(yml_file))[:updated])
-				right_now = Time.parse(data[:updated_at])
-				if (back_then < right_now)
-					U.write_yaml(yml_file, source)
-				end
-			end rescue U.write_yaml(yml_file, source)
-			#TODO debugging purpose only
-			U.write_yaml(yml_file, source) #erase this after
-		end
-	end
+      #the file path which we are working on
+      yml_file = "%s/%08i.yml" % [@parse_file, data[:id]]
+      #if the file was not saved before save it
+      #if it was saved before and the saved timestamp is older than the new one, change it
+      #caching is handled by last update :)
+      begin
+        back_then = Time.parse(YAML::load(File.open(yml_file))[:updated])
+        right_now = Time.parse(data[:updated_at])
+        if (back_then < right_now)
+          U.write_yaml(yml_file, source)
+        end
+      end rescue U.write_yaml(yml_file, source)
+      #TODO debugging purpose only
+      U.write_yaml(yml_file, source) #erase this after
+    end
+  end
 
 end
